bfbedf577d4a597ef85a6688a695ec7737b94c5f
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2021-09-04 19:13:18 +0200

    only insert into DB if not already given

--- Presets/savePreset.php
+++ Presets/savePreset.php
@@ -5,14 +5,34 @@ $json_data = file_get_contents('php://input');
 $color_obj = json_decode($json_data);
 
 try {
-  $stmt = $DB->prepare("INSERT INTO colors (red, green, blue, alpha) VALUE (:red, :blue, :green, :alpha)");
+  $stmt = $DB->prepare("SELECT COUNT(pk_color_id) AS equal_colors FROM colors WHERE (red = :red) AND (green = :green) AND (blue = :blue) AND (alpha = :alpha)");
   $stmt->bindParam(":red", $color_obj->red, PDO::PARAM_INT);
   $stmt->bindParam(":green", $color_obj->green, PDO::PARAM_INT);
   $stmt->bindParam(":blue", $color_obj->blue, PDO::PARAM_INT);
   $stmt->bindParam(":alpha", $color_obj->alpha, PDO::PARAM_STR);
-  $stmt->execute();
+
+  if ($stmt->execute()) {
+    $equal_colors = $stmt->fetch();
+    if ($equal_colors[0] == 0) {
+      try {
+        $stmt = $DB->prepare("INSERT INTO colors (red, green, blue, alpha) VALUE (:red, :green, :blue, :alpha)");
+        $stmt->bindParam(":red", $color_obj->red, PDO::PARAM_INT);
+        $stmt->bindParam(":green", $color_obj->green, PDO::PARAM_INT);
+        $stmt->bindParam(":blue", $color_obj->blue, PDO::PARAM_INT);
+        $stmt->bindParam(":alpha", $color_obj->alpha, PDO::PARAM_STR);
+        $stmt->execute();
+        $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch (PDOException  $e) {
+        print("Error: " . $e);
+        exit();
+      }
+    } else {
+      print("Color already exists!");
+    }
+  }
+
   $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException  $e) {
-  echo "Error: " . $e;
+  print("Error: " . $e);
   exit();
 }
